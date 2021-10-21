fb37222c6446f483fede8e1a480865a2383385f6
yankikirlikova05 <yanki.kirlikova@gmail.com>
2021-10-21 21:57:09 +0300

    drive added to autonom period

--- src/main/java/frc/robot/commands/Auto3Ball.java
+++ src/main/java/frc/robot/commands/Auto3Ball.java
@@ -7,17 +7,17 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Swerve;
-import frc.robot.subsystems.Vision;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Storage;
-
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.Vision;
+import edu.wpi.first.wpilibj.Timer;// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class Auto3Ball extends SequentialCommandGroup {
+  Timer timer = new Timer();
 
   
   /** Creates a new Auto3Ball. */
@@ -33,6 +33,13 @@ public class Auto3Ball extends SequentialCommandGroup {
     RunCommand shooterCommand = new RunCommand(()-> shooter.setShooter(1.0), shooter);
     RunCommand feederCommand = new RunCommand(() -> feeder.runForward(), feeder);
     RunCommand storageCommand = new RunCommand(() -> storage.bothForward(), storage);
+    RunCommand drive = new RunCommand(()-> swerve.drive(0, 0.2, 0, false),swerve){
+    @Override
+    public void end(boolean interrupted){
+      swerve.drive(0, 0, 0, false);
+    }
+
+    };
 
     new AutoAlign(vis,swerve , led);
 
@@ -46,7 +53,8 @@ public class Auto3Ball extends SequentialCommandGroup {
         shooterCommand,
         feederCommand,
         storageCommand
-      ).withTimeout(6)
+      ).withTimeout(6),
+      drive.withTimeout(3)
       );
   }
 
